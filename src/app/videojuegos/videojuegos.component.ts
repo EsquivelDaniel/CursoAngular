e7294111cdeb6e8f566d7b17f8b200a3ed6e03dc
Daniel <careca_esquivel@hotmail.com>
2019-12-30 00:30:45 -0300

    Se agregó una vista

--- src/app/videojuegos/videojuegos.component.ts
+++ src/app/videojuegos/videojuegos.component.ts
@@ -2,24 +2,7 @@ import {Component} from '@angular/core';
 
 @Component ({
     selector: 'videojuegos',
-    template: `
-        <h2> Componente de {{ nombre }} </h2>
-
-        <h3 *ngIf = "mostrarRetro != true"> El mejor juego es: {{ mejorJuego }} </h3>
-
-        <h3 [style.background] = "color"
-            [style.text-align] = "alineacion"
-            *ngIf = "mostrarRetro"> 
-            El mejor juego es: {{ mejorJuegoRetro }}
-        </h3>
-
-        <h2>Listado de Videojuegos</h2>
-        <ul>
-            <li
-                *ngFor = "let game of videojuegos">{{ game }}
-            </li>
-        </ul>
-        `
+    templateUrl: './videojuegos.component.html'
 })
 
 export class VideojuegosComponent{
