c3a0d84dc887c57dc4b854de365d6955c3cdd187
Daniel <careca_esquivel@hotmail.com>
2019-12-30 00:14:55 -0300

    Se agregó la directiva ngIf y ngFor

--- src/app/videojuegos/videojuegos.component.ts
+++ src/app/videojuegos/videojuegos.component.ts
@@ -4,11 +4,36 @@ import {Component} from '@angular/core';
     selector: 'videojuegos',
     template: `
         <h2> Componente de {{ nombre }} </h2>
-        <h3> El mejor juego es: {{ mejorJuego }} </h3>
+
+        <h3 *ngIf = "mostrarRetro != true"> El mejor juego es: {{ mejorJuego }} </h3>
+
+        <h3 [style.background] = "color"
+            [style.text-align] = "alineacion"
+            *ngIf = "mostrarRetro"> 
+            El mejor juego es: {{ mejorJuegoRetro }}
+        </h3>
+
+        <h2>Listado de Videojuegos</h2>
+        <ul>
+            <li
+                *ngFor = "let game of videojuegos">{{ game }}
+            </li>
+        </ul>
         `
 })
 
 export class VideojuegosComponent{
     nombre = 'Videojuegos 2019';
     mejorJuego = 'GTA Vice City';
+    mejorJuegoRetro = 'Super Mario';
+    mostrarRetro = true;
+    color = "blue";
+    alineacion = "center";
+
+    videojuegos = [
+        'Assasins creed',
+        'GTA Vice City',
+        'Call of Duty',
+        'Counter Strike'
+    ];
 }
